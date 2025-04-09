ca0584bae7c91ea2ae8a1f53d84f0574b4dcbcf4
Nure-cs <nuredinabdella459@gmail.com>
2025-04-09 23:15:58 +0300

    Add files via upload

---
+++ Add Digits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            sum_digits = 0
+            while num > 0:
+                sum_digits += num % 10
+                num //= 10
+            num = sum_digits
+        return num
+    
